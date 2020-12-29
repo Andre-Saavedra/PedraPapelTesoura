f563a16b68b46d6062292b86a19b3a4b0c17f762
andre saavedra <andre.saavedra@hotmail.com>
2020-12-29 17:47:49 +0000

    Add project files.

---
+++ PedraPapelTesoura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PedraPapelTesoura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
